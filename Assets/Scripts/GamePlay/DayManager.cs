e65151c65efdd03d808e8d8ba63e47d589025db3
ma.rovere <ma.rovere@gmail.com>
2025-01-26 16:30:10 -0300

    actions applied to player

--- Assets/Scripts/GamePlay/DayManager.cs
+++ Assets/Scripts/GamePlay/DayManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -80,6 +81,7 @@ public class DayManager : MonoBehaviour
 
         switch(actionType)
         {
+            //SIMPLES
             case ActionType.GAIN_BUBBLE:
                 _player.IncrementBubble();
                 break;
@@ -98,6 +100,61 @@ public class DayManager : MonoBehaviour
             case ActionType.LOSE_HIJINKS:
                 _player.UpdateHijinks(-1);
                 break;
+                
+            //GAIN BUBBLES
+            case ActionType.GAIN_BUBBLE_GAIN_HIJINKS:
+                _player.IncrementBubble();
+                _player.UpdateHijinks(1);
+                break;
+            case ActionType.GAIN_BUBBLE_GAIN_MOXIE:
+                _player.IncrementBubble();
+                _player.UpdateMoxie(1);
+                break;
+            case ActionType.GAIN_BUBBLE_LOSE_HIJINKS:
+                _player.IncrementBubble();
+                _player.UpdateHijinks(-1);
+                break;
+            case ActionType.GAIN_BUBBLE_LOSE_MOXIE:
+                _player.IncrementBubble();
+                _player.UpdateMoxie(-1);
+                break;
+
+            //LOSE BUBBLES
+            case ActionType.LOSE_BUBBLE_GAIN_HIJINKS:
+                _player.DecrementBubble();
+                _player.UpdateHijinks(1);
+                break;
+            case ActionType.LOSE_BUBBLE_GAIN_MOXIE:
+                _player.DecrementBubble();
+                _player.UpdateMoxie(1);
+                break;
+            case ActionType.LOSE_BUBBLE_LOSE_HIJINKS:
+                _player.DecrementBubble();
+                _player.UpdateHijinks(-1);
+                break;
+            case ActionType.LOSE_BUBBLE_LOSE_MOXIE:
+                _player.DecrementBubble();
+                _player.UpdateMoxie(-1);
+                break;
+            
+            //NON-BUBBLE
+            case ActionType.GAIN_MOXIE_GAIN_HIJINKS:
+                _player.UpdateHijinks(1);
+                _player.UpdateMoxie(1);
+                break;
+            case ActionType.LOSE_MOXIE_LOSE_HIJINKS:
+                _player.UpdateHijinks(-1);
+                _player.UpdateMoxie(-1);
+                break;
+            case ActionType.GAIN_HIJINKS_LOSE_MOXIE:
+                _player.UpdateHijinks(1);
+                _player.UpdateMoxie(-1);
+                break;
+            case ActionType.GAIN_MOXIE_LOSE_HIJINKS:
+                _player.UpdateHijinks(-1);
+                _player.UpdateMoxie(1);
+                break;
+
             default:
                 break;
         }
