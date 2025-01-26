04dfb376b41c09924380346818163bc432b10a69
ma.rovere <ma.rovere@gmail.com>
2025-01-26 02:28:19 -0300

    random events

--- Assets/Scripts/GamePlay/ConfigManager.cs
+++ Assets/Scripts/GamePlay/ConfigManager.cs
@@ -13,6 +13,7 @@ public class ConfigManager : MonoBehaviour
 
     // DayEvents
     private List<DayAction> _actions;
+    private List<Dialog> _dialogs_available;
     private List<Dialog> _dialogs;
 
     [ContextMenu("SetupConfigData")]
@@ -31,6 +32,9 @@ public class ConfigManager : MonoBehaviour
 
 
     private void CreateDays(){
+        _playerInitialBubbles = 3;
+        _daysToWin = 10;
+
         _actions = new()
         {
             new(1, "Gain bubble", ActionType.GAIN_BUBBLE),
@@ -42,39 +46,23 @@ public class ConfigManager : MonoBehaviour
             new(7, "Do nothing", ActionType.DO_NOTHING)
         };
 
+        _dialogs_available = new();
         _dialogs = new();
+        
         foreach ( var day in dayData.days_list ){            
             var responseAction1 = Enum.Parse<ActionType>(day.ResponseAction1);
             var responseAction2 = Enum.Parse<ActionType>(day.ResponseAction2);
             var day_obj = CreateDialog( day.Id, day.Text, responseAction1, responseAction2 );
-            _dialogs.Add(day_obj);
+            _dialogs_available.Add(day_obj);
         }
-    }
-    private void MockStartUp()
-    {
-        // Mock game configs
-        _playerInitialBubbles = 3;
-        _daysToWin = 10;
 
-        // Mock day events
-        _actions = new()
-        {
-            new(1, "Gain bubble", ActionType.GAIN_BUBBLE),
-            new(2, "Lose bubble", ActionType.LOSE_BUBBLE),
-            new(3, "Gain charisma", ActionType.GAIN_CHARISMA),
-            new(4, "Lose charisma", ActionType.LOSE_CHARISMA),
-            new(5, "Gain chaos", ActionType.GAIN_CHAOS),
-            new(6, "Lose chaos", ActionType.LOSE_CHAOS),
-            new(7, "Do nothing", ActionType.DO_NOTHING)
-        };
-
-        _dialogs = new()
-        {
-            CreateDialog(10, "Dialog Bubble", ActionType.GAIN_BUBBLE, ActionType.LOSE_BUBBLE),
-            CreateDialog(20, "Dialog Charisma", ActionType.GAIN_CHARISMA, ActionType.LOSE_CHARISMA),
-            CreateDialog(30, "Dialog Chaos", ActionType.GAIN_CHAOS, ActionType.LOSE_CHAOS),
-            CreateDialog(40, "Dialog Gain Bubble or Do Nothing", ActionType.GAIN_BUBBLE, ActionType.DO_NOTHING)
-        };
+        for (var i = 0; i < 10; i++){
+            
+            var r = UnityEngine.Random.Range(0, _dialogs_available.Count);
+            var _dialog_type = _dialogs_available[r];
+            Debug.Log(_dialog_type.Text);
+            _dialogs.Add( _dialog_type );
+        }
     }
 
     private Dialog CreateDialog(int Id, string Text, ActionType firstActionType, ActionType secondActionType)
